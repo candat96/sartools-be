5596ae942d5bde12657971333c84822bc506f7ca
Viet Hoang <marcodev.haui@gmail.com>
2023-10-30 09:08:48 +0700

    fix: Prod data

--- src/modules/database/model/prod.entity.ts
+++ src/modules/database/model/prod.entity.ts
@@ -4,11 +4,5 @@ import { BaseEntity } from './base.entity';
 @Entity('prod')
 export class Prod extends BaseEntity {
   @Column({ nullable: true })
-  typo: string;
-
-  @Column({ nullable: true })
-  colonne1: string;
-
-  @Column({ nullable: true })
-  dluo: number;
+  produit: string;
 }
--- src/modules/database/seed/data/prod.data.ts
+++ src/modules/database/seed/data/prod.data.ts
@@ -1,122 +1,17 @@
 export const Prods = [
   {
-    typo: 'Peintures WT Blanches < 300 kg',
-    colonne1: null,
-    dluo: 12,
+    produit: 'Peinture Solvantée'
   },
   {
-    typo: 'Peintures WT Blanches ≥ 300 kg',
-    colonne1: null,
-    dluo: 8,
+    produit: "Peinture à L'eau"
   },
   {
-    typo: 'Ecolack 5 kg',
-    colonne1: null,
-    dluo: 24,
+    produit: 'Peinture Réactive'
   },
   {
-    typo: 'Ecolack ≥ 5 kg',
-    colonne1: null,
-    dluo: 12,
+    produit: 'Enduit à Froid'
   },
   {
-    typo: "Peintures à l'eau Blanches < 300 kg",
-    colonne1: null,
-    dluo: 12,
-  },
-  {
-    typo: "Peintures à l'eau Blanches ≥ 300 kg",
-    colonne1: null,
-    dluo: 8,
-  },
-  {
-    typo: "Peintures à l'eau blanches sans odeur",
-    colonne1: null,
-    dluo: 8,
-  },
-  {
-    typo: 'HYDRAL sauf RAL',
-    colonne1: '1023, 9017',
-    dluo: 10,
-  },
-  {
-    typo: 'HYDRAL RAL',
-    colonne1: '1023, 9017',
-    dluo: 8,
-  },
-  {
-    typo: "Peintures à l'eau Jaunes",
-    colonne1: null,
-    dluo: 8,
-  },
-  {
-    typo: 'Peintures réactives Liq/Sol Comp A',
-    colonne1: null,
-    dluo: 12,
-  },
-  {
-    typo: 'Peintures réactives Liq/Sol Comp B',
-    colonne1: null,
-    dluo: 6,
-  },
-  {
-    typo: 'Peintures réactives Liq/Liq Comp A',
-    colonne1: null,
-    dluo: 12,
-  },
-  {
-    typo: 'Peintures réactives Liq/Liq Comp B',
-    colonne1: null,
-    dluo: 8,
-  },
-  {
-    typo: 'Enduits à Froid blancs',
-    colonne1: null,
-    dluo: 12,
-  },
-  {
-    typo: "Enduits à l'eau",
-    colonne1: null,
-    dluo: 6,
-  },
-  {
-    typo: 'Agripp Colors sauf RAL',
-    colonne1: '3009, 6018, 6024, 7043, 9017 Comp A',
-    dluo: 10,
-  },
-  {
-    typo: 'Agripp Colors RAL',
-    colonne1: '3009, 6018, 6024, 7043, 9017 Comp A',
-    dluo: 4,
-  },
-  {
-    typo: 'CITEFLEX ou ROLLFLEX sauf RAL',
-    colonne1: '3009, 6018, 6024, 7043, 9017 Comp A',
-    dluo: 8,
-  },
-  {
-    typo: 'CITEFLEX ou ROLLFLEX RAL',
-    colonne1: '3009, 6018, 6024, 7043, 9017 Comp A',
-    dluo: 4,
-  },
-  {
-    typo: 'Enduits à Chaud Thermos Blancs',
-    colonne1: null,
-    dluo: 24,
-  },
-  {
-    typo: 'PEPITE',
-    colonne1: null,
-    dluo: 4,
-  },
-  {
-    typo: 'SOLVANTS / DILUANTS',
-    colonne1: null,
-    dluo: 36,
-  },
-  {
-    typo: 'PRIMAIRES / VERNIS',
-    colonne1: null,
-    dluo: 36,
-  },
+    produit: 'Enduit à Chaud'
+  }
 ];
