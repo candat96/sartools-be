ebef010703cfb894ea9246717bb6946822bda22e
Viet Hoang <marcodev.haui@gmail.com>
2023-12-18 17:44:17 +0700

    feat: Jwt

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const Config = {
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || '',
   JWT_SECRET: process.env.JWT_SECRET || 's@cret',
-  JWT_EXPIRED_TIME: 30 * 24 * 60 * 60,
+  JWT_EXPIRED_TIME: 60,
   DATABASE: {
     DATABASE_HOST: process.env.DATABASE_HOST || 'localhost',
     DATABASE_PORT: +process.env.DATABASE_PORT
